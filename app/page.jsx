859833c4f9bf4d1b9730112ce80823c2a840a4e7
siddhantchaudhary09 <siddhantchaudhary096@gmail.com>
2024-12-23 00:22:38 +0530

    complete homapage design

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,14 @@
 import HeroSection from "@/components/Hero";
+import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
-import { featuresData, howItWorksData, statsData } from "@/data/landing";
+import {
+  featuresData,
+  howItWorksData,
+  statsData,
+  testimonialsData,
+} from "@/data/landing";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -57,6 +65,57 @@ export default function Home() {
               </div>
             ))}
           </div>
+        </div>
+      </section>
+
+      <section className="py-20">
+        <div className="container mx-auto px-4">
+          <h2 className="text-3xl font-bold text-center mb-12">
+            What Our Users Say
+          </h2>
+          <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            {testimonialsData.map((test, index) => (
+              <Card key={index} className="p-6">
+                <CardContent className="pt-4">
+                  <div className="flex items-center mb-4">
+                    <Image
+                      src={test.image}
+                      alt={test.name}
+                      width={40}
+                      height={40}
+                      className="rounded-full"
+                    />
+
+                    <div className="ml-4">
+                      <div className="font-semibold">{test.name}</div>
+                      <div className="text-sm text-gray-600">{test.role}</div>
+                    </div>
+                  </div>
+                  <p className="text-gray-600"> {test.quote}</p>
+                </CardContent>
+              </Card>
+            ))}
+          </div>
+        </div>
+      </section>
+      <section className="py-20 bg-blue-600">
+        <div className="container mx-auto px-4 text-center">
+          <h2 className="text-3xl font-bold text-center mb-4">
+            Ready to Take Control of Your Finances?
+          </h2>
+          <p className="text-blue-100 mb-8 max-w-2xl mx-auto">
+            Join thousands of users who are already managing their expenses
+            smarter with Spendly.
+          </p>
+          <Link href={"/dashboard"}>
+            <Button
+              size="lg"
+              className="bg-white text-blue-600 hover:bg-blue-50 animate-bounce"
+            >
+              {" "}
+              Start Free Trial
+            </Button>
+          </Link>
         </div>
       </section>
     </div>
